64db619992177b0250053b2e815602d428993941
nitishpalthya <nitishpalthya@gmail.com>
2026-08-09 17:01:52 +0530

    Configure frontend for production

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL:
+        import.meta.env.VITE_API_URL ||
+        "http://127.0.0.1:8000",
 });
 
 export default api;
